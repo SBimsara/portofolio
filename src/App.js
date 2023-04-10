62d8812ac3bc5b5390c6e01afd4d855a3286e04a
Mad Man <113254019+SBimsara@users.noreply.github.com>
2023-04-11 02:47:20 +0530

    created function to handle icon toggle

--- src/App.js
+++ src/App.js
@@ -3,8 +3,38 @@ import "./styles/styles.css";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome,faUser,faBriefcase,faNewspaper,faEnvelopeOpen } from '@fortawesome/free-solid-svg-icons'
+import { useEffect } from 'react';
 
 function App() {
+
+  const sections = document.querySelectorAll('.section');
+  const sectBtns = document.querySelectorAll('.controls');
+  const sectBtn = document.querySelectorAll('.control');
+  const allSections = document.querySelectorAll('.main-content');
+
+  // function PageTransition() {
+
+  //   //Button click active class
+  //   for(let i=0; i<sectBtn.length; i++) {
+  //     sectBtn[i].addEventListener('click',(event) => {
+  //       let currentBtn = document.querySelectorAll('.active-btn');
+  //       currentBtn[0].classList = currentBtn[0].className.replace ('active-btn', '');
+  //       this.className += 'active-btn';
+  //     })
+  //   }
+  // }
+
+  function handleToggleIcon(event) {
+    const clickedControl = event.target;
+    const controls = document.querySelectorAll('.control');
+    controls.forEach((control) => {
+      if (control !== clickedControl) {
+        control.classList.remove('active-btn');
+      }
+    });
+    clickedControl.classList.add('active-btn');
+  }
+
   return (
     <div className="main-content">
       <header className='section sec1 header active'>
@@ -19,12 +49,12 @@ function App() {
 
       </main>
 
-      <div className='controls'>
+      <div className='controls' onClick={handleToggleIcon}>
 
-        <div className='control control-1 active-btn' data-id= 'header'>
+        <div className='control control-1 active-btn' data-id= 'header' >
           <i><FontAwesomeIcon icon={faHome} /></i>
         </div>
-        <div className='control control-2' data-id= 'about'>
+        <div className='control control-2' data-id= 'about' >
           <i><FontAwesomeIcon icon={faUser} /></i>
         </div>
         <div className='control control-3' data-id= 'portofolio'>
