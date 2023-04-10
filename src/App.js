4dff3d86943e0881ed0bc60660c1db6258df5da7
Mad Man <113254019+SBimsara@users.noreply.github.com>
2023-04-11 01:59:21 +0530

    created nav-icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
-import './App.css';
+import "./styles/styles.css";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome,faUser,faBriefcase,faNewspaper,faEnvelopeOpen } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="main-content">
+      <header className='section sec1 header active'>
+    
       </header>
+      <main>
+
+        <section className='section sec2 about'> </section>
+        <section className='section sec3 portofolio'></section>
+        <section className='section sec4 blog'></section>
+        <section className='section sec5 contact'></section>
+
+      </main>
+
+      <div className='controls'>
+
+        <div className='control control-1 active-btn' data-id= 'header'>
+          <i><FontAwesomeIcon icon={faHome} /></i>
+        </div>
+        <div className='control control-2' data-id= 'about'>
+          <i><FontAwesomeIcon icon={faUser} /></i>
+        </div>
+        <div className='control control-3' data-id= 'portofolio'>
+          <i><FontAwesomeIcon icon={faBriefcase} /></i>
+        </div>
+        <div className='control control-4' data-id= 'blogs'>
+          <i><FontAwesomeIcon icon={faNewspaper} /></i>
+        </div>
+        <div className='control control-5' data-id= 'contact'>
+          <i><FontAwesomeIcon icon={faEnvelopeOpen} /></i>
+        </div>
+
+      </div>
+      
     </div>
   );
 }
