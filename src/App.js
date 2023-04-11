54d49d93d70e7bcd75445e694935e74a751cf97e
Mad Man <113254019+SBimsara@users.noreply.github.com>
2023-04-11 05:51:32 +0530

    styled image and self-intro

--- src/App.js
+++ src/App.js
@@ -52,13 +52,12 @@ function App() {
 
           <div className='right-header'>
             <h1 className='name'>
-              Hi,I'm <span>Sithira Bimsara</span>
-              A University Student
+              Hi, I'm <span>Sithira Bimsara</span>
+              <br/>A University Student
             </h1>
             <p>
             I am an IT university student with a passion for technology and a solid foundation in programming, database management, network security, and web development. 
             I am a continuous learner who keeps up to date with the latest trends in the field. 
-            I have experience working on real-world projects and collaborating with others. 
             My portfolio showcases my technical skills and creativity, and I look forward to discussing my experience and achievements with you.
             </p>
 
